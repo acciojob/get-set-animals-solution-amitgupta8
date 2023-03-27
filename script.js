a3ad16232d6e674b687aee32b9ab19db7bca3029
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-27 15:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ class Animal {
   constructor(species) {
     this._species = species;
   }
-
+  
   get species() {
     return this._species;
   }
-
+  
   makeSound() {
-    console.log("The " + this.species + " makes a sound");
+    console.log("The " + this._species + " makes a sound");
   }
 }
 
@@ -23,8 +23,3 @@ class Dog extends Animal {
     console.log("woof");
   }
 }
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
